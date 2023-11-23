5c4c8d36c179760fa6d7b3499fd8be11780a89cb
Neha <nehaputhran3108@gmail.com>
2023-11-23 16:06:24 +0530

    hehe

--- api/register.php
+++ api/register.php
@@ -1,51 +1,49 @@
 <?php
-    include 'connect.php';
-    // echo "</pre>";print_r($_POST);
-    // exit;
+    include_once 'connect.php';
 
-//     if(isset($image ) ||isset($temp_name ))
-// {
     @$name = $_POST['name'];
     @$mobile = $_POST['mobile'];
     @$password = $_POST['password'];
     @$comfirm = $_POST['comfirm'];
     @$usn = $_POST['usn'];
-    @$image = $_FILES['name']['photo'];
-    @$temp_name = $_FILES['temp_name']['photo'];
+    @$image = $_FILES['photo']['name'];
+    @$temp_name = $_FILES['photo']['tmp_name'];
     @$role = $_POST['role'];
 
-    if($password==$comfirm)
-    {
-                move_uploaded_file($temp_name, "../uploads/$image");
-                $insert = mysqli_query($con, "INSERT INTO user (name, mobile, password, usn, photo, role, status, votes) VALUES ('$name','$mobile','$password','$usn','$image','$role',0,0)");
-                if($insert)
-                {
-                    echo '
-                    <script>
-                    alert("Registration Successful");
-                    window.location= "../index.html";
-                    <script> 
-                    ';
-                }
-                else
-                {
-                    echo '
-                    <script>
-                    alert("Some error occured!");
-                    window.location= "../routes/registration.html";
-                    <script> 
-                    ';
-                }
+    if($password == $comfirm) {
+        if ($_FILES['photo']['error'] === UPLOAD_ERR_OK) {
+            $destination = "../uploads" . basename($image);
 
+            if (move_uploaded_file($temp_name, $destination)) {
+                $insert = mysqli_query($con, "INSERT INTO user (name, mobile, password, usn, photo, role, status, votes) VALUES ('$name','$mobile','$password','$usn','$image','$role',0,0)");
+
+                if($insert) {
+                    echo '<script>
+                            alert("Registration Successful");
+                            window.location= "../index.html";
+                          </script>';
+                } else {
+                    echo '<script>
+                            alert("Some error occurred!");
+                            window.location= "../routes/registration.html";
+                          </script>';
+                }
+            } else {
+                echo '<script>
+                        alert("Error moving uploaded file to destination");
+                        window.location.href = "../routes/registration.html";
+                      </script>';
+            }
+        } else {
+            echo '<script>
+                    alert("Error uploading file");
+                    window.location.href = "../routes/registration.html";
+                  </script>';
+        }
+    } else {
+        echo '<script>
+                alert("Password does not match with confirm password"); 
+                window.location.href = "../routes/registration.html";
+              </script>';
     }
-    else
-    {
-        echo '
-        <script>
-           alert("Password doesnot macth with confirm password");
-           window.location= "../routes/registration.html";
-        <script> 
-        ';
-    }
-// }    
 ?>
